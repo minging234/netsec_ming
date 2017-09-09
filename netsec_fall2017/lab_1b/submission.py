23aa43a5dc13745877c486682620d7898c4ead93
minging234 <825876887@qq.com>
2017-09-08 20:54:33 -0400

    v3

--- netsec_fall2017/lab_1b/submission.py
+++ netsec_fall2017/lab_1b/submission.py
@@ -40,6 +40,8 @@ def basicUnitTest():
     packet1a = RequestColorpackage.Deserialize(packet1Bytes)
     if packet1 == packet1a:
         print("these two RequestColorpackage are same")
+    else:
+        print('wrong')
     
     packet2 = ColorCodepackage()
     packet2.ID = 1
@@ -48,6 +50,8 @@ def basicUnitTest():
     packet2a = ColorCodepackage.Deserialize(packet2Bytes)
     if packet2 == packet2a:
         print("these two ColorCodepackage are same")
+    else:
+        print('wrong')
     
     packet3 = Decodepackage()
     packet3.ID = 1
@@ -57,7 +61,9 @@ def basicUnitTest():
     packet3Bytes = packet3.__serialize__()
     packet3a = Decodepackage.Deserialize(packet3Bytes)
     if packet3 == packet3a :
-        print("these two ColorCodepackage are same")
+        print("these two Decodepackage are same")
+    else:
+        print('wrong')
     
     packet4 = Resultpackage()
     packet4.ID = 3
@@ -66,7 +72,19 @@ def basicUnitTest():
     packet4a = Resultpackage.Deserialize(packet4Bytes)
     if packet4a == packet4 :
         print("these two Resultpackage are same")
+    else:
+        print("wrong")
+    
+    pstream = packet1Bytes + packet2Bytes + packet3Bytes + packet4Bytes
+    deserializer = PacketType.Deserializer()
+    deserializer.update(pstream)
+    for pkt in deserializer.nextPackets():
+        if isinstance(pkt,RequestColorpackage): print('color request')
+        elif isinstance(pkt, ColorCodepackage): print('color code')
+        elif isinstance(pkt, Decodepackage): print('decode color')
+        elif isinstance(pkt, Resultpackage): print('pass fail')
+        else: print("none")
 
-
-basicUnitTest()
+if __name__ == "__main__":
+    basicUnitTest()
 
