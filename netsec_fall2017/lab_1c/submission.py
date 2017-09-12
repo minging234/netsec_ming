322c36c200faf32a687d4f6c709ed21af3ba688e
minging234 <825876887@qq.com>
2017-09-12 11:53:11 -0400

    v_c.3

--- netsec_fall2017/lab_1c/submission.py
+++ netsec_fall2017/lab_1c/submission.py
@@ -1,40 +1,93 @@
 from playground.asyncio_lib.testing import TestLoopEx
 from playground.network.testing import MockTransportToProtocol
+from playground.network.packet.fieldtypes import UINT8, STRING, INT16, BOOL
 from playground.network.packet import PacketType
 import asyncio
-import packageclass
+
+
+class RequestColorpackage(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_xym.Requestcolorpackage"
+    DEFINITION_VERSION = "1.0"
+    FIELDS = [
+]
+
+
+class ColorCodepackage(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_xym.ColorCodepackage"
+    DEFINITION_VERSION = "1.0"
+    FIELDS = [
+              ("ID", UINT8), ("colorcode", STRING)
+              ]
+
+
+class Decodepackage(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_xym.Decodepackage"
+    DEFINITION_VERSION = "1.0"
+    FIELDS = [
+              ("ID", UINT8), ("value1", INT16), ("value2", INT16), ("value3", INT16)
+              ]
+
+
+class Resultpackage(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_xym.Resultpackage"
+    DEFINITION_VERSION = "1.0"
+    FIELDS = [
+              ("ID", UINT8), ("passfail", BOOL)
+              ]
 
 
 class ClolorClientPro(asyncio.Protocol):
-    def __init__(self,loop):
+    def __init__(self, loop):
+        self.state = 'none'
+        self.id = 0
+        self.colorcode = 'none'
+        self.vlue1 = 0
+        self.vlue2 = 0
+        self.vlue3 = 0
         self.loop = loop
         self.transport = None
     
-    def connection_made(self, transport):
-        packet1 = packageclass.RequestColorpackage()
+    def sendfirst(self):
+        packet1 = RequestColorpackage()
         packet1a = packet1.__serialize__()
-        self.transport = transport
+        self.state = 'r_colorcode'
         self.transport.write(packet1a)
-        print('client: color request sent')
+    
+    def passcode(self):
+        return self.colorcode
+    
+    def senddecode(self,v1, v2, v3):
+        packet1 = Decodepackage()
+        packet1.ID = self.id
+        packet1.value1 = v1
+        packet1.value2 = v2
+        packet1.value3 = v3
+        packet1bytes = packet1.__serialize__()
+        self.transport.write(packet1bytes)
+    
+    def connection_made(self, transport):
+        self.transport = transport
+        self.state = 'connected'
     
     def data_received(self, data):
         deserializer = PacketType.Deserializer()
         deserializer.update(data)
         for pkt in deserializer.nextPackets():
-            if isinstance(pkt, packageclass.ColorCodepackage):
+            if isinstance(pkt, ColorCodepackage) and self.state == 'r_colorcode':
                 print('client: color code received')
-                packet1 = packageclass.Decodepackage()
-                packet1.ID = pkt.ID
-                packet1.value1 = int(pkt.colorcode[:2], 16)
-                packet1.value2 = int(pkt.colorcode[2:4], 16)
-                packet1.value3 = int(pkt.colorcode[4:], 16)
-                packet1bytes = packet1.__serialize__()
-                self.transport.write(packet1bytes)
-            # print('decode color send')
-            elif isinstance(pkt, packageclass.Resultpackage):
+                self.colorcode = pkt.colorcode
+                self.id = pkt.ID
+                # self.value1 = int(pkt.colorcode[:2], 16)
+                # self.value2 = int(pkt.colorcode[2:4], 16)
+                # self.value3 = int(pkt.colorcode[4:], 16)
+                self.state = 'r_result'
+            elif isinstance(pkt, Resultpackage) and self.state == 'r_result':
                 print('client: pass fail received')
+                self.state = 'complete'
             else:
-                print('client: none package matched')
+                print('client: none expect package matched')
+                self.transport = None
+                self.loop.stop()
 
 def connection_lost(self, exc):
     self.loop.stop()
@@ -43,7 +96,10 @@ def connection_lost(self, exc):
 
 
 class ColorServerPro(asyncio.Protocol):
-    def __init__(self, cid, message):
+    def __init__(self):
+        self.state = 'none'
+    
+    def findcolor(self, cid, message):
         self.id = cid
         self.message = message
         self.r = int(message[:2], 16)
@@ -52,6 +108,7 @@ class ColorServerPro(asyncio.Protocol):
     
     def connection_made(self, transport):
         self.transport = transport
+        self.state = 'connected'
         peername = transport.get_extra_info('peername')
         print('Connection from:{}'.format(peername))
     
@@ -59,39 +116,57 @@ class ColorServerPro(asyncio.Protocol):
         deserializer = PacketType.Deserializer()
         deserializer.update(data)
         for pkt in deserializer.nextPackets():
-            if isinstance(pkt, packageclass.RequestColorpackage):
+            if isinstance(pkt, RequestColorpackage) and self.state == 'connected':
                 print('server: color request received')
-                packet1 = packageclass.ColorCodepackage()
+                packet1 = ColorCodepackage()
                 packet1.ID = self.id
                 packet1.colorcode = self.message
                 packet1bytes = packet1.__serialize__()
+                self.state = 'r_decode'
                 self.transport.write(packet1bytes)
             # print('color data send')
-            elif isinstance(pkt, packageclass.Decodepackage):
+            elif isinstance(pkt, Decodepackage) and self.state == 'r_decode':
                 print('server: decode color received')
                 if pkt.ID == self.id and pkt.value1 == self.r and pkt.value2 == self.g and pkt.value3 == self.b:
-                    packet2 = packageclass.Resultpackage()
+                    packet2 = Resultpackage()
                     packet2.ID = self.id
                     packet2.passfail = True
                     packet2Bytes = packet2.__serialize__()
+                    self.state = 'complete'
                     self.transport.write(packet2Bytes)
-            # print('pass fail status send')
-            # self.transport.close()
             else:
-                print('server: none package matched')
+                print('server: none expect package matched')
 
 
 def basicUnitTest():
-    cid = 1
-    message = 'A52A2A'
     loop = TestLoopEx()
     asyncio.set_event_loop(loop)
     client = ClolorClientPro(loop)
-    server = ColorServerPro(cid, message)
+    server = ColorServerPro()
     transportToServer = MockTransportToProtocol(server)
     transportToClient = MockTransportToProtocol(client)
     server.connection_made(transportToClient)
     client.connection_made(transportToServer)
+    
+    assert server.state == 'connected'
+    assert client.state == 'connected'
+    
+    cid = 1
+    message = 'A52A2A'
+    server.findcolor(cid, message)
+    client.sendfirst()
+    
+    assert server.state == 'r_decode'
+    assert client.state == 'r_result'
+    
+    colorhex = client.passcode()
+    R = int(colorhex[:2], 16)
+    G = int(colorhex[2:4], 16)
+    B = int(colorhex[4:], 16)
+    client.senddecode(R, G, B)
+    
+    assert server.state == 'complete'
+    assert server.state == 'complete'
 
 if __name__ == "__main__":
     basicUnitTest()
