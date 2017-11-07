1a3038b2c06e54907431737dbdfdd8fdec7fef6a
minging234 <825876887@qq.com>
2017-11-07 00:15:13 -0500

    start 2

--- start.sh
+++ start.sh
@@ -17,49 +17,6 @@ case $1 in
         exit
     ;;
 
-    "run" )
-        case $2 in
-            "packettest" )
-                python -m netsec_fall2017.lab_$3.submission
-                exit
-            ;;
-
-            "crypt" )
-                python -m netsec_fall2017.lab_3.test.crypt
-                exit
-            ;;
-
-            "server" )
-                python -m netsec_fall2017.lab_1d.server
-                exit
-            ;;
-
-            "es" )
-                python -m netsec_fall2017.lab_2.test.EchoServer
-                exit
-            ;;
-
-            "client" )
-                if [[ $# == 2 ]]
-                then
-                    mode="26.1.22.9"
-                else
-                    mode=$3
-                fi
-                python -m netsec_fall2017.lab_1d.client ${mode}
-                exit
-            ;;
-
-            "ec" )
-                python -m netsec_fall2017.lab_2.test.EchoClient
-                exit
-            ;;
-
-            * ) echo "Incorrect args"
-        esac
-        exit
-    ;;
-
     * ) echo "Incorrect args"
 
 esac
